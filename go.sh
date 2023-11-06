45156c25a8c852ef7e3441c1311ec0b0faaa1cfd
120DEV <120@dev.nc>
2023-11-06 16:18:27 +0100

    verbose

--- go.sh
+++ go.sh
@@ -1,3 +1,10 @@
 sudo docker-compose down -v 
-sudo docker-compose up -d --build --force-recreate 
+sudo docker-compose up -d --build --force-recreate
+# Attendre que PostgreSQL soit prêt
+echo "En attente de la disponibilité de PostgreSQL..."
+until sudo docker exec postgres_benchmark pg_isready -U postgres > /dev/null 2>&1; do
+  sleep 1
+  echo -n "."
+done
+echo "PostgreSQL est prêt."
 sudo docker exec postgres_benchmark /scripts/postgres.sh
